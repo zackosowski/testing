5ba0cce24a11ee4a76503e6cfee62d7e1b5215d7
zackosowski <ZACHARYO@MYSTMA.ORG>
2020-09-17 20:11:01 +0000

    top 10 movies

---
+++ unit/src/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("1. Lord of the Rings Trilogy");
+        System.out.println("2. Pacific Rim");
+        System.out.println("3. The Prestige");
+    }
+    
+
+}
